a0db3ad66c0b29bbd92f313d34b024b469002e3c
Hira7388 <mine7388@gmail.com>
2025-07-14 16:58:43 +0900

    [ Feat ] 캐릭터,캐릭터_스텟_클래스 정진규 커밋

--- ConsoleTextRPG/Data/CharacterStat.cs
+++ ConsoleTextRPG/Data/CharacterStat.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleTextRPG.Data
 {
     internal class CharacterStat
     {
+        public int Level { get; private set; }
+        public int MaxHp { get; private set; }
+        public int CurrentHp { get; private set; }
+        public int Attack { get; private set; }
+        public int Defense { get; private set; }
+
+        // 사망 여부를 알려주는 bool형 변수 프로퍼티
+        public bool IsDead => CurrentHp <= 0;
+
+        public CharacterStat(int level, int atk, int def, int hp)
+        {
+            Level = level;
+            Attack = atk;
+            Defense = def;
+            MaxHp = hp;
+            CurrentHp = hp; // 처음에는 체력이 가득 찬 상태
+        }
+
+        // 데미지를 계산하고 적의 공격 데미지와 내 방어력을 계산해서 HP를 변경하는 로직
+        public void ApplyDamage(int damage)
+        {
+            int finalDamage = damage - Defense;
+            if (finalDamage < 1) finalDamage = 1; // 최소 1의 데미지는 받도록 보장
+
+            CurrentHp -= finalDamage;
+            if (CurrentHp < 0) CurrentHp = 0;
+        }
     }
 }
