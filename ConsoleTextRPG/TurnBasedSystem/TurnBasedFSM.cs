96296a94b6826d93a1448861de67a8bc37268e2f
LeeYoungSin <dldudtls100@gmail.com>
2025-07-14 21:52:48 +0900

    add : 턴제시스템 사전세팅

---
+++ ConsoleTextRPG/TurnBasedSystem/TurnBasedFSM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTextRPG.TurnBasedSystem
+{
+    public class TurnBasedFSM : BaseState
+    {
+        public enum State
+        {
+            Idle,
+            Battle,
+            Victory,
+            Size,// Size는 현재 배열의 크기를 시각적으로 나타내주기위한 요소임
+        }
+
+        public override void Enter()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
+
+    }
+}
+
