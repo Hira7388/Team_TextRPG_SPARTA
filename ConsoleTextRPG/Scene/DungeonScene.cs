bd1a2a3cfdd5ed83fbc1ebf72d0a48becd791895
LeeYoungSin <dldudtls100@gmail.com>
2025-07-14 17:04:17 +0900

    feat : DungeonScene화면 구성

--- ConsoleTextRPG/Scene/DungeonScene.cs
+++ ConsoleTextRPG/Scene/DungeonScene.cs
@@ -11,8 +11,6 @@ namespace ConsoleTextRPG.Scene
     {
         // 던전 클리어 조건
         int walkCount = 0; // 이동 횟수
-        int dungeonClearCount = 15; // 던전 클리어 횟수
-        double monsValue = 0.3f; // 몬스터 등장 확률 (30%)
 
         public DungeonScene(GameManager game) : base(game)
         {
@@ -20,12 +18,48 @@ namespace ConsoleTextRPG.Scene
 
         public override void Render()
         {
-            throw new NotImplementedException();
+            Print("◎던전◎", ConsoleColor.Red);
+            Print("3가지 선택지를 보고 길을 선택해주세요\n");
+            Print("이동횟수 : ", walkCount, ConsoleColor.DarkGreen);
+            Print("\n");
+            Print(1, "왼쪽길", ConsoleColor.DarkCyan);
+            Print(2, "앞으로", ConsoleColor.DarkCyan);
+            Print(3, "오른쪽길", ConsoleColor.DarkCyan);
+
+            Print("원하시는 행동을 입력해주세요");
+            Console.Write(">>");
         }
 
         public override void Update()
         {
-            throw new NotImplementedException();
+            string input = Console.ReadLine();
+            int index;
+            if (!int.TryParse(input, out index))
+            {
+                Info("잘못된 입력입니다.");
+                Thread.Sleep(800); 
+                return;
+            }
+            switch (index)
+            {
+                case 1:
+                    Info("앞으로 전진합니다");
+                    Thread.Sleep(500);
+                    break;
+                case 2:
+                    Info("왼쪽길로 갑니다");
+                    Thread.Sleep(500);
+                    break;
+                case 3:
+                    Console.WriteLine("\ninfo : 오른쪽길로 갑니다.");
+                    Thread.Sleep(500);
+                    break;
+                default:
+                    Console.WriteLine("\ninfo : 잘못 입력 하셨습니다.");
+                    Thread.Sleep(800);
+                    break;
+            }
         }
+
     }
 }
