d5cf7c62bfc50bfd4986c3a94cc240a1abdb5ca7
LeeYoungSin <dldudtls100@gmail.com>
2025-07-14 17:20:03 +0900

    fix : 폴더수정

--- ConsoleTextRPG/Scenes/BaseScene.cs
+++ ConsoleTextRPG/Scenes/BaseScene.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleTextRPG.Scene
+namespace ConsoleTextRPG.Scenes
 {
     public abstract class BaseScene
     {
--- ConsoleTextRPG/Scenes/DungeonScene.cs
+++ ConsoleTextRPG/Scenes/DungeonScene.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleTextRPG.Scene
+namespace ConsoleTextRPG.Scenes
 {
     public class DungeonScene : BaseScene
     {
