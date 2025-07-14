48e1f192617749b688e8b08a0ab01dc95ed5af5f
Hira7388 <mine7388@gmail.com>
2025-07-14 11:12:24 +0900

    초기 커밋.

---
+++ ConsoleTextRPG/SpartaDungeon.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleTextRPG
+{
+    internal class SpartaDungeon
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
