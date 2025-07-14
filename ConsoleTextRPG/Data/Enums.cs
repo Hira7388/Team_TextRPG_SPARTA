e739c9419c1fb1b5075ea2683f5b9c29dcb1e3ec
Hira7388 <mine7388@gmail.com>
2025-07-14 16:26:33 +0900

    [ Feat ] 초기 Enums, GameManager 구성 커밋

---
+++ ConsoleTextRPG/Data/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTextRPG.Data
+{
+    internal class Enums
+    {
+        enum GameState
+        {
+            TownScene, // 타운(마을) 씬
+            StoreScene, // 상점 씬
+            InventoryScene, // 인벤토리 씬
+            DungeonOutScene, // 던전 밖 씬
+            DungeonInScene, // 던전 안 씬
+            ViewStatueScene  // 스텟창 씬
+        }
+    }
+}
---
+++ ConsoleTextRPG/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTextRPG.Managers
+{
+    internal class GameManager
+    { 
+        // 싱글톤
+        private static GameManager _instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new GameManager();
+                return _instance;
+            }
+        }
+
+        // 생성자
+        private GameManager()
+        {
+
+        }
+    }
+}
