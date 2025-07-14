a4a296455d33aefaf29636ac2948e46fa112d2f6
jo-ji-hyun <newterr@naver.com>
2025-07-14 20:01:44 +0900

    [Feat]상점 완성

--- ConsoleTextRPG/Data/Item.cs
+++ ConsoleTextRPG/Data/Item.cs
@@ -15,21 +15,21 @@ namespace ConsoleTextRPG.Data
         }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Comment { get; set; }
         public ItemType Type { get; set; }
         public string StatType { get; set; }
         public int StatusBonus { get; set; }
+        public string Comment { get; set; }
         public int Price { get; set; }
         // 아이템이 장착되어 있는지 여부를 추적하는 속성
         public bool IsEquipped { get; set; }
 
-        public Item(int id, string name, ItemType type, string itemType, int statusBonus, string comment, int price)
+        public Item(int id, string name, ItemType type, int statusBonus, string comment, int price)
         {
             Id = id;
             Name = name;
-            Comment = comment;
             Type = type;
             StatusBonus = statusBonus;
+            Comment = comment;
             Price = price;
             IsEquipped = false;
 
